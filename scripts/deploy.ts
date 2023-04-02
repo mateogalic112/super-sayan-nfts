882a0e3ca65ff1b2ae82b3c210022ccf887f5d26
Mateo Galić <mateogalic112@gmail.com>
2023-04-02 13:47:26 +0200

    Update deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,20 +4,22 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying with address: ", deployer.address);
 
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
+  const whitelistContractDeploy = await whitelistContract.deploy(10);
+  await whitelistContractDeploy.deployed();
+  console.log(`SuperSayan deployed to ${whitelistContractDeploy.address}`);
+
   const baseUri =
     "ipfs://bafybeibeppt6l46borcfoinfzhavjjsvew7upenttai75yvmfxl3hmp7oy";
 
   const SuperSayanNFT = await ethers.getContractFactory("SuperSayanNFT");
   const superSayanNFTDeploy = await SuperSayanNFT.deploy(baseUri);
-
   await superSayanNFTDeploy.deployed();
-
   console.log(`SuperSayan deployed to ${superSayanNFTDeploy.address}`);
 
   const Market = await ethers.getContractFactory("Market");
   const marketDeploy = await Market.deploy();
   await marketDeploy.deployed();
-
   console.log(`Market deployed to ${marketDeploy.address}`);
 }
 
