04e1f54d860c3a659fa2c7de7b582bacb6faf89f
Mateo Galić <mateogalic112@gmail.com>
2023-04-08 22:05:07 +0200

    Update main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,14 @@
 
 import Link from "next/link";
 import Inventory from "../components/Inventory";
-import MintButton from "../components/MintButton";
 import NftContainer from "../components/NftContainer";
 import classes from "./index.module.scss";
 
 export default function Home() {
   return (
     <main className={classes.main}>
-      <h1>Welcome to Super Sayan Dapp!</h1>
       <Link href="/whitelist">Join Whitelist</Link>
       <Link href="/mint">Mint Nfts!</Link>
-      <MintButton />
       <NftContainer />
       <Inventory />
     </main>
