b7c0c59cc2b9e01c82021b2decbdcf6fc22adb7f
Mateo Galić <mateogalic112@gmail.com>
2023-04-08 23:47:43 +0200

    Remove extra code

--- components/Whitelist/JoinWhitelistButton.tsx
+++ components/Whitelist/JoinWhitelistButton.tsx
@@ -5,21 +5,13 @@ import useJoinWhitelist from "./hooks/useJoinWhitelist";
 
 const JoinWhitelistButton = () => {
   const { data: joinedWhitelist } = useCheckHasJoinedWhitelist();
-
   const joinWhitelist = useJoinWhitelist();
-  const handleJoin = async () => {
-    try {
-      await joinWhitelist.mutateAsync();
-    } catch (err) {
-      console.log({ err });
-    }
-  };
 
   if (joinedWhitelist) {
     return <p>Joined! 🫡</p>;
   }
 
-  return <button onClick={handleJoin}>Join</button>;
+  return <button onClick={() => joinWhitelist.mutateAsync()}>Join</button>;
 };
 
 export default JoinWhitelistButton;
